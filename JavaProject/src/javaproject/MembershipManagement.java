f23c69e7b3a41d72f22a9bb33b869d9ba81b7e55
amihali1 <andymihalik@gmail.com>
2022-03-24 22:10:48 -0600

    Added MembershipManagement class

---
+++ JavaProject/src/javaproject/MembershipManagement.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaproject;
+
+import java.util.InputMismatchException;
+import java.util.LinkedList;
+import java.util.Scanner;
+/**
+ *
+ * @author rvc
+ */
+public class MembershipManagement
+{
+    final private Scanner reader = new Scanner(System.in);
+    
+    private int getIntInput()
+    {
+        int value = 0;
+        System.out.println("Enter an integer value:");
+        try
+        {
+            value = reader.nextInt();
+        } 
+        catch (Exception e)
+        {
+            System.out.println("Error: You did not enter an integer");
+            getIntInput();
+        }
+        return value;
+    }
+}
