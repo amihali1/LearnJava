bc847276cdd5cd33f0ac2da44491e3d213f34d85
amihali1 <andymihalik@gmail.com>
2022-04-11 22:02:01 -0500

    Added MembershipManagement methods

--- JavaProject/src/javaproject/MembershipManagement.java
+++ JavaProject/src/javaproject/MembershipManagement.java
@@ -22,13 +22,58 @@ public class MembershipManagement
         try
         {
             value = reader.nextInt();
+            
+            if (value == 0)
+                throw new InputMismatchException();
+            
             reader.nextLine();
         } 
         catch (Exception e)
         {
-            System.out.println("Error: You did not enter an integer");
-            getIntInput();
+            System.out.println("Error: You did not enter an integer. Please try again:");
+            value = getIntInput();
         }
         return value;
+    }
+    
+    private void printClubOptions()
+    {
+        System.out.println("1) Club Mercury\n2) Club Neptune\n3) Club Jupiter\n4) Multi Clubs");
+    }
+    
+    private int getChoice()
+    {        
+        System.out.println("WELCOME TO OZONE FITNESS CENTER");
+        System.out.println("===============================");
+        System.out.println("1) Add Member");
+        System.out.println("2) Remove Member");
+        System.out.println("3) Display Member Information\n");
+        System.out.println("Please select an option (or entier -1 to quit):");
+        
+        return getIntInput();               
+    }
+    
+    public String addMembers(LinkedList<Member> members)
+    {
+        String name;
+        int club = 0;
+        String mem;
+        double fees;
+        int memberId;
+        Member member;
+        Calculator<Integer> cal;
+        
+        System.out.print("Enter the member's name: ");
+        name = reader.nextLine();
+        
+        while (club < 1 || club > 4)
+        {
+            printClubOptions();
+            System.out.print("Enter the club ID that the member has access to: ");
+            club = getIntInput();          
+        }
+
+        
+        
     }
 }
