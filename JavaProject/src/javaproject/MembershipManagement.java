c3b4c580892e36e39f1ef53cb30ea2a0da4d35c4
amihali1 <andymihalik@gmail.com>
2022-04-11 23:00:43 -0500

    Finished removeMember()

--- JavaProject/src/javaproject/MembershipManagement.java
+++ JavaProject/src/javaproject/MembershipManagement.java
@@ -19,21 +19,24 @@ public class MembershipManagement
     {
         int value = 0;
         System.out.println("Enter an integer value:");
-        try
+        
+        while (value == 0)
         {
-            value = reader.nextInt();
-            
-            if (value == 0)
-                throw new InputMismatchException();
-            
-            reader.nextLine();
-        } 
-        catch (Exception e)
-        {
-            System.out.println("Error: You did not enter an integer. Please try again:");
-            value = getIntInput();
+           try
+            {
+                value = reader.nextInt();
+
+                if (value == 0)
+                    throw new InputMismatchException();
+
+                reader.nextLine();
+            } 
+            catch (Exception e)
+            {
+                System.out.println("Error: You did not enter an integer. Please try again:");
+            }
         }
-        return value;
+        return value;         
     }
     
     private void printClubOptions()
@@ -56,11 +59,9 @@ public class MembershipManagement
     public String addMembers(LinkedList<Member> members)
     {
         String name;
-        int club = 0;
-        String mem;
+        int club = 0;        
         double fees;
-        int memberId;
-        Member member;
+        int memberId;       
         Calculator<Integer> cal;
         
         System.out.print("Enter the member's name: ");
@@ -73,7 +74,52 @@ public class MembershipManagement
             club = getIntInput();          
         }
 
+        memberId = !members.isEmpty()? members.getLast().getMemberID() + 1 : 1;       
         
+        if (club != 4)
+        {
+            cal = (Integer n) -> switch (n) 
+            {
+                case 1 -> 900;
+                case 2 -> 950;
+                case 3 -> 1000;
+                default -> -1;
+            };
+                    
+            fees = cal.calculateFees(club);
+            members.add(new SingleClubMember('S',memberId, name, fees, club));
+            System.out.println("\nSTATUS: Single Club Member added\n");
+        }
+        else
+        {
+            cal = (Integer n) -> switch (n) 
+            {
+                case 4 -> 1200;
+                default -> -1;
+            };
+            
+            fees = cal.calculateFees(club);            
+            members.add(new MultiClubMember('M', memberId, name, fees, 100));
+            System.out.println("\nSTATUS: Multi Club Member added\n");
+        }
         
+        return members.getLast().toString();
+    }
+    
+    public void removeMember(LinkedList<Member> members)
+    {        
+        System.out.println("Enter the Member ID of the member you want to remove: ");
+        var memberId = getIntInput();
+        
+        for (int i = 0; i < members.size(); i++)
+        {
+            if (members.get(i).getMemberID() == memberId)
+            {
+                members.remove(i);
+                System.out.println("Member " + memberId + " has been removed.");
+                return;
+            }
+        }
+        System.out.println("Member ID " + memberId + " not found.");
     }
 }
